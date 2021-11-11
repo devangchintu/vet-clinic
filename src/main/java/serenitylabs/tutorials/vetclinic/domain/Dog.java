780c373273114d688be53d2f36ef1064be602686
Rachchh, Devang <Devang.Rachchh@allscripts.com>
2021-11-11 16:04:22 +0530

    Comminting changes

--- src/main/java/serenitylabs/tutorials/vetclinic/domain/Dog.java
+++ src/main/java/serenitylabs/tutorials/vetclinic/domain/Dog.java
@@ -1,15 +1,33 @@
 package serenitylabs.tutorials.vetclinic.domain;
 
+
+import javax.lang.model.type.NullType;
+import java.awt.*;
+import java.security.cert.CertPathBuilder;
+import java.time.LocalDateTime;
+
 public class Dog {
+
     private final String name;
-    private final String breed;
-    private final String colour;
+    private final String Breed;
+    private final LocalDateTime birthday;
+    private final String favoriteFood;
+    private final String color;
 
-    public Dog(String name, String breed, String colour) {
 
+
+    public Dog(String name, String breed, LocalDateTime birthday, String favoriteFood, String Color) {
         this.name = name;
-        this.breed = breed;
-        this.colour = colour;
+        this.Breed= breed;
+        this.birthday = birthday;
+
+        this.favoriteFood = favoriteFood;
+        this.color = Color;
+    }
+
+
+    public String getColor() {
+        return color;
     }
 
     public String getName() {
@@ -17,20 +35,34 @@ public class Dog {
     }
 
     public String getBreed() {
-        return breed;
+        return  Breed;
     }
 
-    public String getColour() {
-        return colour;
+    public LocalDateTime getBirthday() {
+        return birthday;
     }
 
-    public static DogBuilder called(String name) {
+    public static WithBreed called(String name ) {
         return new DogBuilder(name);
     }
 
-    public static class DogBuilder {
+    public String getFavoriteFood() {
+        return  favoriteFood;
+    }
+
+    interface WithBreed {
+        OfColor ofBreed(String breed);
+    }
+
+   interface OfColor{
+       DogBuilder OfColor(String Color);
+   }
+    public static class DogBuilder implements WithBreed ,OfColor{
         private final String name;
         private String breed;
+        private LocalDateTime birthday;
+        private String favoriteFood;
+        private String Color;
 
         public DogBuilder(String name) {
             this.name = name;
@@ -41,8 +73,21 @@ public class Dog {
             return this;
         }
 
-        public Dog andOfColour(String colour) {
-            return new Dog(name, breed, colour);
+        public Dog bornon(LocalDateTime birthday){
+            this.birthday = birthday;
+                return new Dog(name, breed, birthday,favoriteFood,Color);
+            }
+
+        public DogBuilder withFavoriteFood(String favoriteFood) {
+
+            this.favoriteFood = favoriteFood;
+            return this;
+        }
+
+        public DogBuilder OfColor(String Color) {
+            this.Color = Color;
+            return this;
         }
     }
-}
+    }
+
